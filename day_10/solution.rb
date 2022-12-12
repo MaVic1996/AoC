0280cfbd94b18e35a0ce59431d54d3ea5cd7260f
Víctor M. Chaves García <victormcg96@gmail.com>
2022-12-12 22:48:41 +0100

    Add solution for day 10

--- day_10/solution.rb
+++ day_10/solution.rb
@@ -5,24 +5,52 @@ def solve_1(file_path)
   calculate_signal_strength(20, 40, lines).sum
 end
 
+def solve_2(file_path)
+  lines = File.read(file_path).split("\n")
+  draw_CRT(lines)
+end
+
 def calculate_signal_strength(start_metric, steps, commands)
   x_val = 1
   cycle_val = 0
   signal_measures = []
   for idx in 0..commands.length
     command = commands[idx]
-    puts command if command.nil?
+    cycle_val += 1
+    if (cycle_val-start_metric) % steps == 0
+      signal_measures << cycle_val * x_val 
+    end
     if !command.nil? && command.include?('addx')
       cycle_val += 1
       if (cycle_val-start_metric) % steps == 0
-      signal_measures << cycle_val * x_val if (cycle_val-start_metric) % steps == 0
+        signal_measures << cycle_val * x_val if (cycle_val-start_metric) % steps == 0
+      end
+      x_val = x_val + command.split(' ')[1].to_i
     end
   end
-  cycle_val += 1
-  if (cycle_val-start_metric) % steps == 0
-    signal_measures << cycle_val * x_val 
-  end
-  x_val = x_val + command.split(' ')[1].to_i if !command.nil? && command.include?('addx')
-  end
   signal_measures
+end
+
+def draw_CRT(commands)
+  x_val = 1
+  cycle_val = 0
+  crt_line = 40.times.map{|_| '.'}
+  for idx in 0..commands.length
+    crt_line[cycle_val%40] = '#' if [x_val-1, x_val, x_val+1].include?(cycle_val%40)
+    command = commands[idx]
+    cycle_val += 1    
+    if cycle_val%40 == 0
+      puts crt_line.join('')
+      crt_line = 40.times.map{|_| '.'}
+    end
+    if !command.nil? && command.include?('addx')
+      crt_line[cycle_val%40] = '#' if [x_val-1, x_val, x_val+1].include?(cycle_val%40)
+      cycle_val += 1
+      if cycle_val%40 == 0
+        puts crt_line.join('')
+        crt_line = 40.times.map{|_| '.'}
+      end
+      x_val = x_val + command.split(' ')[1].to_i
+    end
+  end
 end
