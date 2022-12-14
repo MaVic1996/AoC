2c5d2d1d12b4123556333a0bfbd3416f40531622
Víctor M. Chaves García <victormcg96@gmail.com>
2022-12-15 00:23:00 +0100

    Add day 11 solution

--- day_11/solution.rb
+++ day_11/solution.rb
@@ -2,7 +2,29 @@
 
 def solve_1(file_path = 'day_11/example.txt')
   monkey_list = File.read(file_path).gsub("\r",'').split("\n\n").map{|data| parse_monkey_data(data)}
-  monkey_list
+  lam_f = lambda {|wr| wr/3 }
+  max_1, max_2 = pass_rounds(monkey_list, 20, lam_f).values.max(2)
+  max_1 * max_2
+end
+
+def solve_2(file_path = 'day_11/example.txt')
+  monkey_list = File.read(file_path).gsub("\r",'').split("\n\n").map{|data| parse_monkey_data(data)}
+  lam_f = lambda {|wr| wr % monkey_list.map(&:condition).reduce{|a,b| lcm(a,b)} }
+  max_1, max_2 = pass_rounds(monkey_list, 10000, lam_f).values.max(2)
+  max_1 * max_2
+end
+
+def pass_rounds(monkey_list, rounds, worry_level)
+  inspect_map = monkey_list.map{|m| [m.id, 0]}.to_h
+  rounds.times do |round|
+    monkey_list.each do |monkey|
+      inspect_map[monkey.id] = inspect_map[monkey.id] + monkey.items.count
+      monkey.inspect_items(worry_level).map do |next_monkey, item|
+        monkey_list.select{|m| m.id == next_monkey}.first.items << item
+      end
+    end
+  end
+  inspect_map
 end
 
 def parse_monkey_data(data)
@@ -31,11 +53,27 @@ class Monkey
   end
 
   def parse_worry_operation(worry_operation)
-    worry_operation
+    worry_operation.split("new = ")[1]
   end
 
   def check_condition(condition)
     condition
+  end
+
+  def inspect_items(wl_calc)
+    monkey_item = []
+    items.each do |item|
+      old = item
+      new_worry_level = eval(worry_operation)
+      new_worry_level = wl_calc.call(new_worry_level)
+      if new_worry_level % condition == 0
+        monkey_item << [id_true, new_worry_level]
+      else
+        monkey_item << [id_false, new_worry_level]
+      end
+    end
+    @items = []
+    monkey_item
   end
 
 
